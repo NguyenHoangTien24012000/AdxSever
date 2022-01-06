c7bda7670195300b7b99b9d62ddff1fccb86b61d
NguyenHoangTien24012000 <hoangtienflytom@gmail.com>
2022-01-06 17:47:08 +0700

    day3

--- src/controller/UserAdminController.js
+++ src/controller/UserAdminController.js
@@ -1,18 +1,21 @@
-const { type, json } = require('express/lib/response');
+// const { type, json } = require('express/lib/response');
+// const { type } = require('express/lib/response');
 const connection = require('../services/connectDB')
 const { make } = require('../services/JWT')
 class UserAdminController {
     checkLogin = async (req, res) => {
-        let {user} = req.body;
-        console.log(JSON.parse(user));
-        let {email, password} = JSON.parse(user);
-        // console.log(typeof email)
-        if (!email || !password) {
+        // let {data} = req.body;
+        // console.log("data", req.body);
+        let data = req.body;
+        let {user, password} = data;
+        // console.log(email, password)
+      
+        if (!user || !password) {
             return res.status(401).json({
                 message: 'missing required params',
             })
         }
-        const [rows, fields] = await connection.execute('SELECT * FROM user_admin WHERE email = ? AND password_login = ?', [email, password]);
+        const [rows, fields] = await connection.execute('SELECT * FROM user_admin WHERE email = ? AND password_login = ?', [user, password]);
         if (rows.length === 0) {
             return res.status(401).json({
                 message: 'Login failed'
@@ -20,7 +23,7 @@ class UserAdminController {
         }
 
         if(rows.length > 0){
-            const _token = await make(user);
+            const _token = await make(data);
             return res.status(200).send({
                 token: _token,
                 message: 'ok'
--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ const port = 5000;
 const initWebRoute = require('./routes/index.route')
 const authMiddleWare = require('./services/authMiddleWare');
 const userLoginRouter = require('./routes/loginIndex.route');
+// const cors = require('cors');
 // const router = express.Router();
+
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control_Allow_Methods", "GET, POST, DELETE, PUT");
+    next();
+  });
+
 
 //init web route
 
