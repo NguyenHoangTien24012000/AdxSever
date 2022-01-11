c0b31ebbe7aa74c6c9d2ae616ea4a6162af1570d
NguyenTien2401 <hoangtienflytom@gmail.com>
2022-01-11 09:44:09 +0700

    day 7

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const port = 5000;
 const initWebRoute = require('./routes/index.route')
 const authMiddleWare = require('./services/authMiddleWare');
 const userLoginRouter = require('./routes/loginIndex.route');
+const multer = require('multer');
+// const { path } = require('express/lib/application');
 
-const multer  = require('multer')
-const upload = multer({ dest: 'access/' })
+
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,12 +18,37 @@ app.use(function (req, res, next) {
     res.header("Access-Control_Allow_Methods", "GET, POST, DELETE, PUT");
     next();
 });
+app.use(express.static('uploads'));
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
 
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 app.post('/profile', upload.single('image'), function (req, res, next) {
     console.log(req.file)
     // req.body will hold the text fields, if there were any
-  })
+})
 
 
 //init web route
