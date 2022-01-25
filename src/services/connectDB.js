637fde61cf7895e5b2d18d3cab24b876f513da97
NguyenTien2401 <hoangtienflytom@gmail.com>
2022-01-25 12:22:04 +0700

    first commit

--- src/services/connectDB.js
+++ src/services/connectDB.js
@@ -2,10 +2,12 @@ const mysql = require('mysql2/promise');
 
 // create the connection to database
 const connection =  mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'dataadx',
-  password : 'hoangtien14'
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b0c880b56c3ba4',
+  password: '6c62b14e',
+  database : 'heroku_a20f59501d2e9b9'
 });
 
 module.exports = connection;
+
+// mysql://b0c880b56c3ba4:6c62b14e@us-cdbr-east-05.cleardb.net/heroku_a20f59501d2e9b9?reconnect=true
